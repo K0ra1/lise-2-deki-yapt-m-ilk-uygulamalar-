f3b7d5f557cf3e53708d15b919da5c5cb0eb61eb
K0ra1 <kryykt1416@gmail.com>
2025-12-02 12:22:41 +0300

    Add files via upload

---
+++ RefOut/RefOut/Matematik.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefOut
+{
+    internal class Matematik
+    {
+        public void Artir(int x)
+        {
+            x++;
+        }
+        public void Artirout(out  int x)
+        {
+            x = 123;
+            x++;
+        }
+        public void Artirref (ref int x)
+        {
+            x = 123;
+
+        }
+    }
+}
