660ae37ba8df0cd149ce63779a02d81336f9f171
K0ra1 <kryykt1416@gmail.com>
2025-12-02 09:02:16 +0300

    Add files via upload

---
+++ 10. Sınıf Nesne Tabanlı Programlama 29.uygulama/29.Uygulama/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _29.Uygulama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int say = 1;
+            listBox1.Items.Clear();
+            do
+            {
+                listBox1 .Items.Add(say);
+                say++;
+            }
+            while (say <= 10);
+
+                   
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Form2 x = new Form2();
+            x.Show();
+        }
+    }
+}
