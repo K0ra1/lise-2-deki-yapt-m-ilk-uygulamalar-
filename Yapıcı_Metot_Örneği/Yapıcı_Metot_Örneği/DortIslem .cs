58c4c9be130d14e757f4346c7bd840c8b672fd79
K0ra1 <kryykt1416@gmail.com>
2025-12-02 16:00:09 +0300

    Add files via upload

---
+++ Yapıcı_Metot_Örneği/Yapıcı_Metot_Örneği/DortIslem .cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yapıcı_Metot_Örneği
+{
+    internal class DortIslem
+    {
+        public int sayi1;
+        public int sayi2;
+
+        public DortIslem()
+        {
+            {
+                this.sayi1 = sayi1;
+                this.sayi2 = sayi2;
+            }
+
+        }
+        public DortIslem(int sayi1)
+        {
+            this.sayi1 = sayi1;
+            this.sayi2 = 3;
+
+        }
+        public DortIslem(int sayi1, int sayi2) 
+        {
+            this.sayi1 = sayi1;
+            this.sayi2 = sayi2;
+            
+        }
+        public int Topla ()
+        {
+            return this.sayi1 + this.sayi2;
+
+        }
+        public int Carp()
+        {
+            return this.sayi1 - this.sayi2;
+        }
+    }
+}
+
+
+
+
+
---
+++ Yapıcı_Metot_Örneği/Yapıcı_Metot_Örneği/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Yapıcı_Metot_Örneği
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DortIslem islem = new DortIslem();
+            label3.Text = Convert.ToString(islem.Topla();
+            label4.Text=Convert.ToString(islem.Carp());
+
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            DortIslem dortIslem = new DortIslem();
+            islem.sayi1 = Convert.ToInt32(textBox1.Text);
+            label3.Text = Convert.ToString(dortIslem.Topla);
+             abel
+        }
+    }
+}-
