cd3519544504f154a6018b8cfefd8b3c5df5cf57
K0ra1 <kryykt1416@gmail.com>
2025-12-02 08:55:40 +0300

    Add files via upload

---
+++ Buyuk_Olanı_Bul/Buyuk_Olanı_Bul/Buyuk_olanı_bul .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buyuk_Olanı_Bul
+{
+    internal class Buyuk_olanı_bul
+    {
+        public int Bul(int a, int b)
+        {
+            int sonuc;
+            if (a < b)
+            {
+                return sonuc = b;
+            }
+            else
+            {
+                return sonuc = a;
+            }
+
+        }
+    }
+}
---
+++ Buyuk_Olanı_Bul/Buyuk_Olanı_Bul/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buyuk_Olanı_Bul
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sayi1,sayi2;
+            Buyuk_olanı_bul BuyukBul= new Buyuk_olanı_bul();
+            sayi1 = Convert .ToInt32(textBox1.Text);    
+            sayi2 = Convert .ToInt32(textBox2.Text);
+            textBox3.Text=BuyukBul.Bul(sayi1 ,sayi2).ToString();  
+
+
+
+        }
+    }
+}
